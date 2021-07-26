2d4cfd1341d85b4a994475df1c55d068a3aec3da
nguyentantrung2000 <trung.nt4996@sinhvien.hoasen.edu.vn>
2021-07-26 15:43:03 +0700

    pull

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import 'rmwc/dist/styles';
 import { HomePage } from './pages/home_page/HomePage';
 import { NavBar } from './components/nav_bar/Navbar';
+import {Personaldetail} from './components/personal_detail/Personaldetail'
 import firebase from 'firebase';
 import { config } from './firebase.config';
 
@@ -10,10 +11,10 @@ firebase.initializeApp(config);
 
 function App() {
   return (
-    <>
+    <div>
       <NavBar></NavBar>
-      {/* <HomePage></HomePage> */}
-    </>
+      <Personaldetail/>
+    </div>
   );
 }
 
---
+++ src/pages/create_cv/createCV.tsx
@@ -0,0 +1,5 @@
+import './createCV.css'
+
+export function createCV(){
+    
+}
