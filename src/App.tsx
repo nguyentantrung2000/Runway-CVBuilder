d5b9a49b496580ea36e713151985e3e58b7c37da
nguyenxuanquang30061999 <quang.nx0628@sinhvien.hoasen.edu.vn>
2021-07-28 13:42:13 +0700

    full UI dialog

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,15 @@ import { HomePage } from './pages/home_page/HomePage';
 import { NavBar } from './components/nav_bar/Navbar';
 import firebase from 'firebase';
 import {config} from './firebase.config';
-
+import {CreateSkill} from './components/create_skill_popup/create_skill' 
 firebase.initializeApp(config);
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
-      <HomePage></HomePage>
+      {/* <HomePage></HomePage> */}
+      <CreateSkill></CreateSkill>
     </>
   );
 }
