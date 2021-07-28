ef09b6e1c10be18651367185978f127fc631dc0c
nguyentantrung2000 <trung.nt4996@sinhvien.hoasen.edu.vn>
2021-07-28 11:02:15 +0700

    pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import './App.css';
 import 'rmwc/dist/styles';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { HomePage } from './pages/home_page/HomePage';
-import {CreateCV} from './pages/create_cv/CreateCV';
+import { CreateCV } from './pages/create_cv/CreateCV';
 import { NavBar } from './components/nav_bar/Navbar';
 import { UserCV } from './pages/UserCV/UserCV';
 import firebase from 'firebase';
 import { config } from './firebase.config';
-import {ViewExampleCV} from './pages/ViewExampleCV/ViewExampleCV'
-import {CreateCV2} from './pages/create_cv2/CreateCV2'
+import { ViewExampleCV } from './pages/ViewExampleCV/ViewExampleCV'
+import { CreateCV2 } from './pages/create_cv2/CreateCV2'
 firebase.initializeApp(config);
 
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <NavBar></NavBar>
-      {/* <CreateCV2/> */}
-      {/* <CreateCV/> */}
-      {/* <ViewExampleCV></ViewExampleCV> */}
-      <UserCV></UserCV>
-    </div>
+      <Router>
+        <Switch>
+          <Route path="/createcv" component={CreateCV} />
+          <Route path="/createcv2" component={CreateCV2} />
+          <Route path="/usercv" component={UserCV} />
+          <Route path="/viewexcv" component={ViewExampleCV} />
+        </Switch>
+      </Router>
+
+
+    </React.Fragment>
   );
 }
 
