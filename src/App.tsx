b9fd24d0e45ca4fac60af680c0dc76370b834e35
nguyentantrung2000 <trung.nt4996@sinhvien.hoasen.edu.vn>
2021-08-09 10:41:17 +0700

    pull

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,38 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { HomePage } from './pages/home_page/HomePage';
 import { NavBar } from './components/nav_bar/Navbar';
 import { UserCV } from './pages/UserCV/UserCV';
-import {ChooseLanguage} from './components/choose_language/choose_language'
+import { ChooseLanguage } from './components/choose_language/choose_language'
 import firebase from 'firebase';
 import { config } from './firebase.config';
 import { ViewExampleCV } from './pages/ViewExampleCV/ViewExampleCV'
 import { CreateCV2 } from './pages/create_cv2/CreateCV2'
-import {CreateCV} from './pages/create_cv/createCV'
+import { CreateCV } from './pages/create_cv/createCV'
 import { LandingPage } from './pages/landing_page/LandingPage';
 import { CV2 } from './pages/CV_Template/CV2/CV2';
 import { CV3 } from './pages/CV_Template/CV3/CV3';
-import {CV1} from './pages/CV_Template/CV1/cv1'
+import { CV1 } from './pages/CV_Template/CV1/cv1'
 firebase.initializeApp(config);
 
 function App() {
   return (
     <>
-     <React.Fragment>
-       <NavBar></NavBar>
-       <Router>
-         <Switch>
-           <Route path="/" exact component={LandingPage}/>
-           <Route path="/createcv" component={CreateCV} />
-           <Route path="/cv2" component={CV2} />
-           <Route path="/createcv2" component={CreateCV2} />
-           <Route path="/usercv" component={UserCV} />
-           <Route path="/viewexcv" component={ViewExampleCV} />
-           <Route path="/cv3" component={CV3} />
-           <Route path="/cv1" component={CV1} />
-         </Switch>
-       </Router>
-     </React.Fragment>
-</>
+      <React.Fragment>
+        <NavBar></NavBar>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={LandingPage} />
+            <Route path="/createcv" component={CreateCV} />
+            <Route path="/createcv2" component={CreateCV2} />
+            <Route path="/usercv" component={UserCV} />
+            <Route path="/viewexcv" component={ViewExampleCV} />
+            <Route path="/cv1" component={CV1} />
+            <Route path="/cv2" component={CV2} />
+            <Route path="/cv3" component={CV3} />
+
+          </Switch>
+        </Router>
+      </React.Fragment>
+    </>
 
   );
 }
