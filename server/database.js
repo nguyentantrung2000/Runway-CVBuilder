f8797ceeaac333d5f6a247c3ab0479bf9a7ab21e
Hoang Trung <trungdoublelift@gmail.com>
2021-08-09 10:42:34 +0700

    .

--- server/database.js
+++ server/database.js
@@ -3,10 +3,34 @@ var admin = require("firebase-admin");
 var serviceAccount = require("path/to/serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://runway-cv-builder-default-rtdb.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://runway-cv-builder-default-rtdb.firebaseio.com"
 });
 
-const firestore=admin.firestore();
+const firestore = admin.firestore();
+
+ class Database{
+
+//Thêm
+
+
+//Xóa
+async  deleteCV(id, CVID) {
+    try{
+        await firestore.collection("Users").doc(id).update({
+            OwnedCV:admin.firestore.FieldValue.delete(CVID),
+        })
+    }catch(err){
+        console.log(err);
+    }
+    return 0;
+}   
+
+//Sửa
+}
+module.exports=Database;
+
+
+
 
 
