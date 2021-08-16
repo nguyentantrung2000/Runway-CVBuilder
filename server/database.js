fb29fcfa1e906db7f48a8dbc6cb9773fb228cf6b
nguyentantrung2000 <trung.nt4996@sinhvien.hoasen.edu.vn>
2021-08-16 13:42:56 +0700

    get edit cv

--- server/database.js
+++ server/database.js
@@ -2,124 +2,126 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require("../server/runway-cv-builder-firebase-adminsdk-e0svh-ab805ce173.json");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://runway-cv-builder-default-rtdb.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://runway-cv-builder-default-rtdb.firebaseio.com"
 });
 
 const firestore = admin.firestore();
 
 class Database {
 
-  //Thêm vào OwnerCV 
-  async addCV(UserID) {
-    try {
-      let temp;
-      let date=new Date().getDate()
-      let month=new Date().getMonth()
-      let year=new Date().getFullYear()
-      await firestore.collection("CV").add({
-        CVDetails: {
-          Fname: null,
-          Lname: null,
-          Email: null,
-          dob: null,
-          phone: null,
-          Address: null,
-          Country: null,
-          Bio: null,
-          Skills: null,
-          Hobbies: null,
-          Educations: null,
-          Employments: null,
-        },
-        CVthumnail: null,
-        CVImage: null,
-        dateCreated:`${month}/${date}/${year}`
+    //Thêm vào OwnerCV 
+    async addCV(UserID) {
+        try {
+            let temp;
+            let date = new Date().getDate()
+            let month = new Date().getMonth()
+            let year = new Date().getFullYear()
+            await firestore.collection("CV").add({
+                CVDetails: {
+                    Fname: null,
+                    Lname: null,
+                    Email: null,
+                    dob: null,
+                    phone: null,
+                    Address: null,
+                    Country: null,
+                    Bio: null,
+                    Skills: null,
+                    Hobbies: null,
+                    Educations: null,
+                    Employments: null,
+                },
+                CVthumnail: null,
+                CVImage: null,
+                dateCreated: `${month}/${date}/${year}`
 
-      }).then(async data => {
-        return temp = data.id;
-      })
-      await firestore.collection("Users").doc(UserID).update({
-        OwnedCV: admin.firestore.FieldValue.arrayUnion(temp),
-      })
-      return 0
+            }).then(async data => {
+                return temp = data.id;
+            })
+            await firestore.collection("Users").doc(UserID).update({
+                OwnedCV: admin.firestore.FieldValue.arrayUnion(temp),
+            })
+            return 0
+        } catch (err) {
+            console.log(err);
+        }
     }
-    catch (err) {
-      console.log(err);
+
+
+    async getAllOwnerCV(UserID) {
+        let CVList;
+        let CVDetailList = [];
+        CVList = await firestore.collection("Users").doc(UserID)
+            .get().then(data => {
+                return data.data().OwnedCV;
+            })
+        for (let i = 0; i < CVList.length; i++) {
+            await firestore.collection("CV").doc(CVList[i]).get().then(data => {
+                return CVDetailList.push(data.data())
+            })
+        }
+        return CVDetailList;
     }
-  }
 
-
-  async getAllOwnerCV(UserID) {
-    let CVList;
-    let CVDetailList = [];
-    CVList = await firestore.collection("Users").doc(UserID)
-      .get().then(data => {
-        return data.data().OwnedCV;
-      })
-    for (let i = 0; i < CVList.length; i++) {
-      await firestore.collection("CV").doc(CVList[i]).get().then(data => {
-        return CVDetailList.push(data.data())
-      })
+    async geUserEdit(CVID) {
+        await firestore.collection("CV").doc(CVID)
+            .get().then(data => {
+                return data.data();
+            })
     }
-    return CVDetailList;
-  }
-  async getCVDetail(CVID) {
-    let temp;
-    await firestore.collection("CV").doc(CVID).get().then(data => {
-      return temp = data.data();
-    })
-    return
-  }
 
-
-
-  //Xóa
-  async deleteCV(id, CVID) {
-    try {
-      await firestore.collection("Users").doc(id).update({
-        OwnedCV: admin.firestore.FieldValue.arrayRemove(CVID),
-      })
-    } catch (err) {
-
+    async getCVDetail(CVID) {
+        let temp;
+        await firestore.collection("CV").doc(CVID).get().then(data => {
+            return temp = data.data();
+        })
+        return
     }
-    return 0;
-  }
-
-  //Sửa
 
 
-  ////Lưu thông tin CV
-  async saveCVInfo(CVID, Fname, Lname, Email, dob, phone, Address, Country, Bio, Skills, Hobbies, Educations, Employments, CVthumnail, CVImage) {
-    try {
-      await firestore.collection("CV").doc(CVID).set({
-        CVDetails: {
-          Fname: Fname || null,
-          Lname: Lname || null,
-          Email: Email || null,
-          dob: dob || null,
-          phone: phone || null,
-          Address: Address || null,
-          Country: Country || null,
-          Bio: Bio || null,
-          Skills: Skills || null,
-          Hobbies: Hobbies || null,
-          Educations: Educations || null,
-          Employments: Employments || null,
-        },
-        CVthumnail: CVthumnail || null,
-        CVImage: CVImage || null,
-      })
-    } catch (err) {
 
+    //Xóa
+    async deleteCV(id, CVID) {
+        try {
+            await firestore.collection("Users").doc(id).update({
+                OwnedCV: admin.firestore.FieldValue.arrayRemove(CVID),
+            })
+        } catch (err) {
+
+        }
+        return 0;
     }
-    return 0;
-  }
+
+    //Sửa
+
+
+    ////Lưu thông tin CV
+    async saveCVInfo(CVID, Fname, Lname, Email, dob, phone, Address, Country, Bio, Skills, Hobbies, Educations, Employments, CVthumnail, CVImage) {
+        try {
+            await firestore.collection("CV").doc(CVID).set({
+                CVDetails: {
+                    Fname: Fname || null,
+                    Lname: Lname || null,
+                    Email: Email || null,
+                    dob: dob || null,
+                    phone: phone || null,
+                    Address: Address || null,
+                    Country: Country || null,
+                    Bio: Bio || null,
+                    Skills: Skills || null,
+                    Hobbies: Hobbies || null,
+                    Educations: Educations || null,
+                    Employments: Employments || null,
+                },
+                CVthumnail: CVthumnail || null,
+                CVImage: CVImage || null,
+            })
+        } catch (err) {
+
+        }
+        return 0;
+    }
 }
 
 module.exports = Database;
-
-
-
-
-
--- server/index.js
+++ server/index.js
@@ -9,67 +9,67 @@ const db = new Database();
 app.use(cors());
 app.use(body.json());
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 });
 
-app.post("/testPostWithBody", async (req, res) => {
-  const { json } = req.body
-  try {
-    console.log(json);
-    res.send(json);
-  } catch (err) {
-    console.log(err);
-  }
+app.post("/testPostWithBody", async(req, res) => {
+    const { json } = req.body
+    try {
+        console.log(json);
+        res.send(json);
+    } catch (err) {
+        console.log(err);
+    }
 
 })
-app.get("/getAPI", async (req, res) => {
-  res.send("Hello from server");
-})
-///////// CV LIST API //////////////////////////////////
-app.post("/deleteCV", async (req, res) => {
-  const { id, CVID } = req.body;
-  try {
-    let result = await db.deleteCV(id, CVID);
-    res.send("Xóa thành công");
-  } catch (err) {
+app.get("/getAPI", async(req, res) => {
+        res.send("Hello from server");
+    })
+    ///////// CV LIST API //////////////////////////////////
+app.post("/deleteCV", async(req, res) => {
+    const { id, CVID } = req.body;
+    try {
+        let result = await db.deleteCV(id, CVID);
+        res.send("Xóa thành công");
+    } catch (err) {
 
-  }
-})
-
-app.post("/addCV", async (req, res) => {
-  const { UserID } = req.body;
-  try {
-    let result = await db.addCV(UserID);
-    res.send("Thêm CV thành công");
-  } catch (err) {
-    
-  }
+    }
 })
 
-app.post("/deleteCV", async (req, res) => {
-  const { id, CVID } = req.body;
-  try {
-    let result = await db.deleteCV(id, CVID);
-    res.send("Xóa thành công");
-  } catch (err) {
+app.post("/addCV", async(req, res) => {
+    const { UserID } = req.body;
+    try {
+        let result = await db.addCV(UserID);
+        res.send("Thêm CV thành công");
+    } catch (err) {
 
-  }
+    }
 })
-///////// END CV LIST API //////////////////////////////////
+
+app.post("/deleteCV", async(req, res) => {
+        const { id, CVID } = req.body;
+        try {
+            let result = await db.deleteCV(id, CVID);
+            res.send("Xóa thành công");
+        } catch (err) {
+
+        }
+    })
+    ///////// END CV LIST API //////////////////////////////////
 
 
 ///////// CV  API //////////////////////////////////
 
-app.post("/saveCV", async (req, res) => {
-  const { CVID, Fname, Lname, Email, dob, phone, Address, Country, Bio, Skills, Hobbies, Educations, Employments,CVthumnail,CVImage } = req.body
-  try {
-    let result = await db.saveCVInfo(CVID, Fname, Lname, Email, dob, phone, Address, Country, Bio, Skills, Hobbies, Educations, Employments,CVthumnail,CVImage);
-    res.send("Lưu thông tin thành công");
-  } catch (err) {
-    console.log(err)
-  }
-})
-///////// END CV  API //////////////////////////////////
+app.post("/saveCV", async(req, res) => {
+        const { CVID, Fname, Lname, Email, dob, phone, Address, Country, Bio, Skills, Hobbies, Educations, Employments, CVthumnail, CVImage } = req.body
+        try {
+            let result = await db.saveCVInfo(CVID, Fname, Lname, Email, dob, phone, Address, Country, Bio, Skills, Hobbies, Educations, Employments, CVthumnail, CVImage);
+            res.send("Lưu thông tin thành công");
+        } catch (err) {
+            console.log(err)
+        }
+    })
+    ///////// END CV  API //////////////////////////////////
 
 ////////// User CV ////////////////////
 // app.post("/userAddCV",async(req,res)=>{
@@ -82,21 +82,30 @@ app.post("/saveCV", async (req, res) => {
 //   }
 // })
 
-app.post("/userAddCV", async (req, res) => {
-  const { UserID, Fname, Lname, Email, dob, phone, Address, Country, Bio, Skills, Hobbies, Educations, Employments } = req.body
-  try {
-    let result = await db.addNewCV(UserID, Fname, Lname, Email, dob, phone, Address, Country, Bio, Skills, Hobbies, Educations, Employments);
-    res.send("Tạo CV mới thành công ");
-  } catch (err) {
-    console.log(err)
-  }
+app.post("/userAddCV", async(req, res) => {
+    const { UserID, Fname, Lname, Email, dob, phone, Address, Country, Bio, Skills, Hobbies, Educations, Employments } = req.body
+    try {
+        let result = await db.addNewCV(UserID, Fname, Lname, Email, dob, phone, Address, Country, Bio, Skills, Hobbies, Educations, Employments);
+        res.send("Tạo CV mới thành công ");
+    } catch (err) {
+        console.log(err)
+    }
 })
-app.post("/getOwnedCV", async (req, res) => {
-  const { UserID } = req.body
-  try {
-    let result = await db.getAllOwnerCV(UserID);
-    res.json(result);
-  } catch (err) {
-    console.log(err)
-  }
+app.post("/getOwnedCV", async(req, res) => {
+    const { UserID } = req.body
+    try {
+        let result = await db.getAllOwnerCV(UserID);
+        res.json(result);
+    } catch (err) {
+        console.log(err)
+    }
+})
+app.post("/getOwnedCV", async(req, res) => {
+    const { CVID } = req.body
+    try {
+        let result = await db.geUserEdit(CVID);
+        res.json(result);
+    } catch (err) {
+        console.log(err)
+    }
 })
